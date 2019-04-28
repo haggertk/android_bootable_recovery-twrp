LineageTWRP/android_bootable_recovery

recovery: Get a proper shell environment when using toybox
* Build a proper mksh version for recovery when not using busybox.
  Current non-busybox shell doesn't have $TMPDIR set, which means
  random breakage can occur like addon.d failing to apply if the
  device is not decrypted, because TMPDIR is not set to /tmp.

Change-Id: I44f2b76c6699f54daa302810ef461e72d66a6d12

Android.mk
@@ -417,6 +417,7 @@ else
     ifneq ($(wildcard external/unzip/Android.mk),)
         LOCAL_ADDITIONAL_DEPENDENCIES += unzip
     endif
+    LOCAL_REQUIRED_MODULES += sh_recovery mkshrc_recovery
 endif
 
 ifneq ($(TW_NO_EXFAT), true)
@@ -783,6 +784,7 @@ include $(commands_recovery_local_path)/injecttwrp/Android.mk \
     $(commands_recovery_local_path)/minzip/Android.mk \
     $(commands_recovery_local_path)/dosfstools/Android.mk \
     $(commands_recovery_local_path)/etc/Android.mk \
+    $(commands_recovery_local_path)/mksh/Android.mk \
     $(commands_recovery_local_path)/toybox/Android.mk \
     $(commands_recovery_local_path)/simg2img/Android.mk \
     $(commands_recovery_local_path)/adbbu/Android.mk \

haggertk/android_bootable_recovery-twrp

Android.mk
@@ -445,6 +445,7 @@ else
     ifneq ($(wildcard external/unzip/Android.mk),)
         LOCAL_REQUIRED_MODULES += unzip
     endif
+    LOCAL_REQUIRED_MODULES += sh_recovery mkshrc_recovery
 endif
 
 ifneq ($(TW_NO_EXFAT), true)
@@ -846,6 +847,7 @@ include $(commands_TWRP_local_path)/injecttwrp/Android.mk \
     $(commands_TWRP_local_path)/minzip/Android.mk \
     $(commands_TWRP_local_path)/dosfstools/Android.mk \
     $(commands_TWRP_local_path)/etc/Android.mk \
+    $(commands_TWRP_local_path)/mksh/Android.mk \
     $(commands_TWRP_local_path)/toybox/Android.mk \
     $(commands_TWRP_local_path)/simg2img/Android.mk \
     $(commands_TWRP_local_path)/adbbu/Android.mk \